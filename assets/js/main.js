30e1a392fcbe61f08950fe40a908ef38daf59c01
Mateusz Konach <konachelk@gmail.com>
2017-02-20 19:54:36 +0100

    paralax effect

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    var $window = $(window);
+
+    $('section[data-type="background"]').each(function () {
+
+        var $bgobj = $(this);
+
+        $(window).scroll(function () {
+
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+            var coords = '50% ' + yPos + 'px';
+            $bgobj.css({
+                backgroundPosition: coords
+            });
+
+            console.log(yPos, coords, $window.scrollTop(), $bgobj.data('speed'));
+
+        });
+    })
+
+});
